03da89d5e531bf67a31f571151ef102b30fae589
rebmizrahi <becca.mizrahi@gmail.com>
2023-02-11 15:42:00 -0500

    get tests

--- Part A/tests/UnitTest.py
+++ Part A/tests/UnitTest.py
@@ -22,11 +22,28 @@ def start_app():
     kill_app()
 
 
+##### HEADERS ####
 
-##### HEADERS #### """
 def test_header():
     response = requests.get('http://localhost:4567')
     assert response.status_code == 200
     assert 'Content-Type' in response.headers
     assert response.headers['Content-Type'] == 'text/html'
 
+
+### GET ###
+
+def test_get_todos():
+    response = requests.get('http://localhost:4567/todos')
+    assert response.status_code == 200
+    assert 'todos' in response.json()
+
+def test_get_projects():
+    response = requests.get('http://localhost:4567/projects')
+    assert response.status_code == 200
+    assert 'projects' in response.json()
+
+def test_get_categories():
+    response = requests.get('http://localhost:4567/categories')
+    assert response.status_code == 200
+    assert 'categories' in response.json()
