b5aca93adb40703270806d52ef842b406667c707
rebmizrahi <becca.mizrahi@gmail.com>
2023-02-11 17:33:21 -0500

    interoperability tests

--- Part A/tests/UnitTest.py
+++ Part A/tests/UnitTest.py
@@ -94,13 +94,37 @@ def test_post_project_json():
 
 def test_post_category_json():
     new_category_json = json.dumps({
-        "title": "Extracurricular",
-        "description": ""
+        "title": "School",
+        "description": "testing"
         })
     response = requests.post('http://localhost:4567/categories',data=new_category_json,headers={"Content-Type": "application/json"})
     assert response.status_code == 201 #Create
     categories = requests.get('http://localhost:4567/categories').json()["categories"]
     assert response.json() in categories
 
+
 ### GET BY ID ###
 
+
+### CATEGORY & TODO RELATIONSHIP ###
+
+def test_get_todos_by_category():
+    response = requests.get('http://localhost:4567/categories/1/todos')
+    todos = requests.get('http://localhost:4567/todos').json()["todos"]
+    todos_in_category = [i for i in todos if 'categories' in i.keys() and {'id': '1'} in i['categories']]
+    
+    assert response.status_code == 200
+
+    #from the original entries, todo 1 is in category 1 and todo 2 is not.
+    print(requests.get('http://localhost:4567/todos/1').json()["todos"])
+    print(todos_in_category)
+    assert requests.get('http://localhost:4567/todos/1').json()["todos"][0] in todos_in_category
+    assert requests.get('http://localhost:4567/todos/2').json()["todos"][0] not in todos_in_category
+
+
+def test_get_categories_by_todos():
+    response = requests.get('http://localhost:4567/todos/1/categories').json()["categories"]
+    category = requests.get('http://localhost:4567/categories/1').json()["categories"][0]
+    #from the original entries, todo 1 has category 1. if more categories are added, 
+    # should still be valid, so just check that category 1 is still in todo 1's categories, not if they are equal
+    assert category in response
