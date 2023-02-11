954dd558a7d6f3b4bf81b17656b771846fe64c3d
rebmizrahi <becca.mizrahi@gmail.com>
2023-02-11 15:12:58 -0500

    tests added

--- Part A/tests/UnitTest.py
+++ Part A/tests/UnitTest.py
@@ -1,9 +1,29 @@
 import pytest
 import requests
 import json
+import subprocess
 
-##### HEADERS ####
+#initial state restored once app is terminated
+@pytest.fixture(scope="session", autouse=True)
+def kill_app():
+    try:
+        process = subprocess.call(['curl', 'http://localhost:4567/shutdown'], shell=True)
+    except ConnectionError:
+        pass
+    
 
+@pytest.fixture(scope='module')
+def start_app():
+    process =  subprocess.Popen(["java", "-jar", "runTodoManagerRestAPI-1.5.5.jar"], shell=True)
+    status_code = subprocess.call(['curl', 'http://localhost:4567'], shell=True)
+    while status_code:
+        status_code = subprocess.call(['curl', 'http://localhost:4567'], shell=True)
+    yield status_code
+    kill_app()
+
+
+
+##### HEADERS #### """
 def test_header():
     response = requests.get('http://localhost:4567')
     assert response.status_code == 200
