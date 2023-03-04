ff5eea57f22a622a5d86a5a8f4d2f4be64576189
LeeBrickman <lee.brickman@mail.mcgill.ca>
2023-03-04 17:29:57 -0500

    Added DeleteProject.py and edited DeleteProject.feature

--- Part B/features/DeleteProject.feature
+++ Part B/features/DeleteProject.feature
@@ -12,13 +12,13 @@ Feature: Delete a Project
   Scenario Outline: Attempting to delete a project by providing the incorrect id
     Given there is no existent project with <incorrectId> in the system
     When  a user elects to delete a project by providing the <incorrectId>
-    Then no project will be removed from the system, and an error message "Could not find any instances with projects/" + "<incorrectId>" is returned
+    Then no project will be removed from the system, and an error message is returned
     Examples:
       |incorrectId|
       |20000000000|
       |90909090909|
   #Error Flow
   Scenario: Attempting to delete a project by providing no id
-    Given there are existent projects in the system
+    Given project(s) exist in the system
     When  a user elects to delete a project, but does not specify a project id
     Then no project will be removed from the system, a "404 Not Found" Error occurs, and the return statement is blank
---
+++ Part B/features/steps/DeleteProject.py
@@ -0,0 +1,98 @@
+import requests
+from behave import *
+import json
+
+url = 'http://localhost:4567/projects'
+
+json_header = {"Content-Type": "application/json"}
+
+test_id = 0
+
+
+@given("project(s) exist in the system")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+
+    project = {
+        "title": "test title project",
+        "description": "test description project",
+        "completed": "false",
+        "active": "false"
+    }
+
+    request = requests.post(url, data=json.dumps(project), headers=json_header)
+
+    global test_id
+    test_id = request.json()["id"]
+
+    assert (request.status_code == 201)
+
+
+@given("there is no existent project with {incorrectId} in the system")
+def step_impl(context, incorrectId):
+    """
+    :type context: behave.runner.Context
+    :type incorrectId: str
+    """
+
+    request = requests.get(url + incorrectId)
+    assert (request.status_code == 404)
+
+
+@when("a user elects to delete a project by correctly providing the project id")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+
+    request = requests.delete(url + "/" + str(test_id))
+    assert (request.status_code == 200)
+
+
+@when("a user elects to delete a project by providing the {incorrectId}")
+def step_impl(context, incorrectId):
+    """
+    :type context: behave.runner.Context
+    :type incorrectId: str
+    """
+
+    request = requests.delete(url + "/" + incorrectId)
+    context.error_request = request.json()
+    assert (request.status_code == 404)
+
+
+@then("the project with the provided id will be removed from the system")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    request = requests.get(url + "/" + str(test_id))
+    assert (request.status_code == 404)
+
+
+@then('no project will be removed from the system, and an error message is returned')
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    assert context.error_request["errorMessages"] is not None
+
+
+@when("a user elects to delete a project, but does not specify a project id")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    request = requests.delete(url+"/:id")
+    context.error_request = request
+
+
+@then('no project will be removed from the system, a "404 Not Found" Error occurs, and the return statement is blank')
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    assert 'Could not find any instances with projects/:id' in context.error_request.json()["errorMessages"]
+    assert context.error_request.status_code == 404
