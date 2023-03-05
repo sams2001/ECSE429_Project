fa528b152cc9577d0bf064861c80f17fef5b8bd8
rebmizrahi <becca.mizrahi@gmail.com>
2023-03-05 16:52:46 -0500

    reorganized

--- Part B/features/steps/FetchCategoryByTodo.py
+++ Part B/features/steps/FetchCategoryByTodo.py
@@ -4,20 +4,7 @@ import json
 
 url= 'http://localhost:4567/'
 json_header = {"Content-Type": "application/json"}
-
-
-@then(u'fetching categories via todo {todoid} will return the category {categoryid} associated with todo {todoid}')
-def step_impl(context,todoid,categoryid):
-    """
-    :type context: behave.runner.Context
-    :type todoid: str
-    :type categoryid: str
-    """
-    response = requests.get(url+'todos/'+todoid+'/categories')
-    assert response.status_code==200
-    response_categories = response.json()['categories']
-    assert categoryid in str(response_categories)
-
+error_stack = []
 
 @given(u'that todo with id {todoid} has a relationship to the categories with ids {categoryids}')
 def step_impl(context,todoid,categoryids):
@@ -43,21 +30,6 @@ def step_impl(context,todoid,categoryids):
             assert categoryid in str(response.json()['categories'])
 
 
-@then(u'fetching categories via todo {todoid} will return all of the categories {categoryids} associated with todo {todoid}')
-def step_impl(context,todoid,categoryids):
-    """
-    :type context: behave.runner.Context
-    :type todoid: str
-    :type categoryids: str
-    """
-    categoryidlst = categoryids.strip().split(',')
-    response = requests.get(url+'todos/'+todoid+'/categories')
-    assert response.status_code==200
-    response_categories = response.json()['categories']
-    for categoryid in categoryidlst:
-        assert categoryid in str(response_categories)
-
-
 @given(u'that categories with ids {categoryids} exists in the system')
 def step_impl(context,categoryids):
     """
@@ -66,10 +38,8 @@ def step_impl(context,categoryids):
     """
     categoryidlst = categoryids.strip().split(',')
     for categoryid in categoryidlst:
-        print(str(categoryid))
         response = requests.get(url+'categories/'+categoryid)
         if response.status_code == 404:
-            print("posting " + str(categoryid))
             json_1 = {
                 "title": "Work",
                 "description": ""
@@ -77,7 +47,6 @@ def step_impl(context,categoryids):
             response1 = requests.post(url+'categories',data=json.dumps(json_1),headers=json_header)
             assert response1.status_code == 201 
             response = requests.get(url+'categories/'+categoryid)
-            print(str(response.status_code))
         assert response.status_code == 200
 
 
@@ -94,3 +63,55 @@ def step_impl(context,todoid):
         assert response.status_code==404
     finally:
         print('ERROR: service fails, fetching categories via a todo that does not exist returns categories')
+
+
+@when(u'fetching categories via todo {todoid}')
+def step_impl(context,todoid):
+    """
+    :type context: behave.runner.Context
+    :type todoid: str
+    """
+    response = requests.get(url+'todos/'+todoid+'/categories')
+    if 'errorMessage' in str(response.json()):
+        error_stack.push(str(response.json()))
+    try:
+        assert response.status_code==200
+    except:
+        assert response.status_code==404
+
+
+@then(u'the category {categoryid} associated with todo {todoid} shall be returned')
+def step_impl(context,categoryid,todoid):
+    """
+    :type context: behave.runner.Context
+    :type categoryid: str
+    :type todoid: str
+    """
+    response = requests.get(url+'todos/'+todoid+'/categories')
+    assert response.status_code==200
+    response_categories = response.json()['categories']
+    assert categoryid in str(response_categories)
+
+
+@then(u'all of the categories {categoryids} associated with todo {todoid} shall be returned')
+def step_impl(context,categoryids,todoid):
+    """
+    :type context: behave.runner.Context
+    :type categoryids: str
+    :type todoid: str
+    """
+    categoryidlst = categoryids.strip().split(',')
+    response = requests.get(url+'todos/'+todoid+'/categories')
+    assert response.status_code==200
+    response_categories = response.json()['categories']
+    for categoryid in categoryidlst:
+        assert categoryid in str(response_categories)
+
+
+@then(u'will return an error message')
+def step_impl(context):
+    try:
+        assert 'errorMessage' in str(error_stack.pop())
+    except:
+        print('ERROR: service fails, fetching categories via a todo that does not exist returns categories')
+        raise False
